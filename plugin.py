d9c862f770a2d8a42abe29bf353de05eb94b9e28
Pascal Drewes <pascal@drewes-webdesign.nl>
2020-10-11 13:08:46 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -9,18 +9,12 @@ Requirements:
 
 """
 """
-<plugin key="SMA" name="Domoticz-SMA-Inverter" version="0.8.0" author="Want100Cookies">
+<plugin key="SMA" name="SMA Solar Inverter (modbus TCP/IP)" version="0.8.0" author="Want100Cookies">
     <params>
         <param field="Address" label="Your SMA IP Address" width="200px" required="true" default="10.10.10.7"/>
         <param field="Port" label="Port" width="40px" required="true" default="502"/>
         <param field="Mode1" label="Device ID" width="40px" required="true" default="3" />
         <param field="Mode2" label="Reading Interval sec." width="40px" required="true" default="5" />
-        <param field="Mode6" label="Debug" width="75px">
-            <options>
-                <option label="True" value="Debug"/>
-                <option label="False" value="Normal"  default="true" />
-            </options>
-        </param>
     </params>
 </plugin>
 """
@@ -34,43 +28,51 @@ def onStart():
     Domoticz.Log("Domoticz SMA Inverter Modbus plugin start")
 
     if 1 not in Devices:
-        Domoticz.Device(Name="Solar Production", Unit=1, Type=243, Subtype=33, SwitchType=4, Used=0).Create()
+        Domoticz.Device(Name="Solar Yield", Unit=1, TypeName="kWh", Used=0).Create()
     if 2 not in Devices:
-        Domoticz.Device(Name="DC Power", Unit=2, TypeName="Usage", Used=0).Create()
+        Domoticz.Device(Name="DC Power A", Unit=2, TypeName="Usage", Used=0).Create()
     if 3 not in Devices:
-        Domoticz.Device(Name="AC Power", Unit=3, TypeName="Usage", Used=0).Create()
+        Domoticz.Device(Name="DC Power B", Unit=3, TypeName="Usage", Used=0).Create()
     if 4 not in Devices:
-        Domoticz.Device(Name="Temperature", Unit=4, TypeName="Temperature", Used=0).Create()
+        Domoticz.Device(Name="AC Power", Unit=4, TypeName="Usage", Used=0).Create()
+    if 5 not in Devices:
+        Domoticz.Device(Name="Temperature", Unit=5, TypeName="Temperature", Used=0).Create()
 
-    Domoticz.Heartbeat(Parameters["Mode2"])
+    global client
+    client = ModbusClient(host=Parameters["Address"], port=Parameters["Port"], unit_id=Parameters["Mode1"])
+    client.open()
+
+    Domoticz.Heartbeat(int(Parameters["Mode2"]))
 
 def onHeartbeat():
     TotalYieldAddress = 30529
-    DCPowerAddress = 30773
+    DCPowerAAddress = 30773
+    DCPowerBAddress = 30961
     ACPowerAddress = 30775
     TemperatureAddress = 30953
 
-    try:
-        client = ModbusClient(host=Parameters["Address"], port=Parameters["Port"], unit_id=Parameters["Mode1"], auto_open=True, auto_close=True)
-        TotalYieldRead = client.read_holding_registers(TotalYieldAddress, 2)
-        DCPowerRead = client.read_holding_registers(DCPowerAddress, 2)
-        ACPowerRead = client.read_holding_registers(ACPowerAddress, 2)
-        TemperatureRead = client.read_holding_registers(TemperatureAddress, 2)
-        
-    except:
-        Domoticz.Log("Connection problem")
+    if not client.is_open():
+        Domoticz.Log("Not connected. Reconnecting...")
+        client.open()
         return
+    
+    TotalYieldRead = client.read_holding_registers(TotalYieldAddress, 2)
+    DCPowerARead = client.read_holding_registers(DCPowerAAddress, 2)
+    DCPowerBRead = client.read_holding_registers(DCPowerBAddress, 2)
+    ACPowerRead = client.read_holding_registers(ACPowerAddress, 2)
+    TemperatureRead = client.read_holding_registers(TemperatureAddress, 2)
 
     TotalYieldValue = BinaryPayloadDecoder.fromRegisters(TotalYieldRead, byteorder=Endian.Big, wordorder=Endian.Big).decode_32bit_uint()
-    DCPowerValue = BinaryPayloadDecoder.fromRegisters(DCPowerRead, byteorder=Endian.Big, wordorder=Endian.Big).decode_32bit_uint()
+    DCPowerAValue = BinaryPayloadDecoder.fromRegisters(DCPowerARead, byteorder=Endian.Big, wordorder=Endian.Big).decode_32bit_uint()
+    DCPowerBValue = BinaryPayloadDecoder.fromRegisters(DCPowerBRead, byteorder=Endian.Big, wordorder=Endian.Big).decode_32bit_uint()
     ACPowerValue = BinaryPayloadDecoder.fromRegisters(ACPowerRead, byteorder=Endian.Big, wordorder=Endian.Big).decode_32bit_uint()
     TemperatureValue = BinaryPayloadDecoder.fromRegisters(TemperatureRead, byteorder=Endian.Big, wordorder=Endian.Big).decode_32bit_uint()
 
-    if Parameters["Mode6"] == 'Debug':
-        Domoticz.Log(f'TotalYield\t{str(TotalYieldValue)}\DCPower\t{str(DCPowerValue)}\tACPower\t{str(ACPowerValue)}\tTemperature\t{str(TemperatureValue)}')
-
-    if DCPowerValue == 2147483648:
-        DCPowerValue = 0
+    if DCPowerAValue == 2147483648:
+        DCPowerAValue = 0
+    
+    if DCPowerBValue == 2147483648:
+        DCPowerBValue = 0
     
     if ACPowerValue == 2147483648:
         ACPowerValue = 0
@@ -78,7 +80,8 @@ def onHeartbeat():
     if TemperatureValue == 2147483648:
         TemperatureValue = 0
     
-    Devices[1].Update(0, f'{str(TotalYieldValue)};{str(ACPowerValue)}')
-    Devices[2].Update(0, str(DCPowerValue))
-    Devices[3].Update(0, str(ACPowerValue))
-    Devices[4].Update(0, str(TemperatureValue))
+    Devices[1].Update(0, str(TotalYieldValue) + ";" + str(ACPowerValue))
+    Devices[2].Update(0, str(DCPowerAValue))
+    Devices[3].Update(0, str(DCPowerBValue))
+    Devices[4].Update(0, str(ACPowerValue))
+    Devices[5].Update(0, str(TemperatureValue / 10))
